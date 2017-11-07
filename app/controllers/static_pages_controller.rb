ebb63f84995a5d3563a3340231301e917422a563
Tim <timothystahl@gmail.com>
2017-11-06 20:50:29 -0700

    create routes CF 5.1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
 
+	def index
+  @products = Product.all
+end
+
   def landing_page
-    @products = Product.limit(3)
-  end
+    @products = Product.limit(4)
+	end
+
+	def contact
+	end
+
+	def about
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
+
   resources :products
-  get 'static_pages/about'
 
-  get 'static_pages/contact'
+  root 'static_pages#index', as: 'intro'
 
-  root 'static_pages#landing_page'
+
+  get 'static_pages/contact', to: 'static_pages#contact', as: 'contact'
+
+  #root 'static_pages#landing_page'
+
+
+  get '/home', to: 'static_pages#landing_page', as: 'home'
+
+  get 'static_pages/about', to: 'static_pages#about', as: 'about'
+
+  get 'static_pages/index', to: 'static_pages#landing_page', as: 'landing_page'
+
+  
+
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
