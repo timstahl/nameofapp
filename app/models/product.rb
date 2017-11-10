e0871557055c4a0c379073a21f2ffbb1df055c4c
Tim <timothystahl@gmail.com>
2017-11-10 11:55:59 -0700

    revisions to CF 5.2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
 
 	def self.search(search_term)
-  	Product.where("name LIKE ?", "%#{search_term}%")
+		like_string = Rails.env.production? ? "ilike" : "LIKE"
+		Product.where("name #{like_string} ?", "%#{search_term}%")
 	end
 
 
