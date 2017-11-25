95f465511804c67c70fe62bff2416c7a30268b8e
Tim <timothystahl@gmail.com>
2017-11-25 13:43:06 -0700

    add comment table to database 5.7

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,18 @@
 class Product < ApplicationRecord
+has_many :comments
+has_many :orders
 
 	def self.search(search_term)
 		like_string = Rails.env.production? ? "ilike" : "LIKE"
 		Product.where("name #{like_string} ?", "%#{search_term}%")
 	end
 
+	def highest_rating_comment
+  	comments.rating_desc.first
+	end
 
+	def lowest_rating_comment
+		comments.rating_asc.first
+	end	
 
 end
