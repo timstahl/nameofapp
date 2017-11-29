5bdbd16b5d57f74a39fd425e6d569d1449742cd9
Tim <timothystahl@gmail.com>
2017-11-29 14:13:07 -0700

    fix comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,7 @@
+
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
-<<<<<<< HEAD
-=======
-
->>>>>>> e9375193fcc9b43b93ea2c21a0a6b8aad6cc686c
-
   scope :rating_desc, -> { order(rating: :desc) }
 
-  scope :rating_asc, -> { order(rating: :asc) }
-
 end
-
-
