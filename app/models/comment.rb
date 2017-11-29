e9375193fcc9b43b93ea2c21a0a6b8aad6cc686c
Tim Stahl <timothystahl@gmail.com>
2017-11-29 08:43:00 -0700

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
-<<<<<<< HEAD
-end
-=======
+
 
   scope :rating_desc, -> { order(rating: :desc) }
 
@@ -12,4 +10,3 @@ end
 end
 
 
->>>>>>> 1c4992f307477a2a4c37133f097c64b9113fa76f
