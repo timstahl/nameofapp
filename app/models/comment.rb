a8d39abac282afc20b9afcff3bcf16e927f6f2e5
Tim <timothystahl@gmail.com>
2017-11-29 08:21:37 -0700

    merge

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
-<<<<<<< HEAD
-end
-=======
 
   scope :rating_desc, -> { order(rating: :desc) }
 
   scope :rating_asc, -> { order(rating: :asc) }
 
 end
-
-
->>>>>>> 1c4992f307477a2a4c37133f097c64b9113fa76f
