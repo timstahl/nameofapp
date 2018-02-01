ab9be735c8cf096520fa52b5a0988743d3c6d2a6
Tim <timothystahl@gmail.com>
2018-02-01 11:56:33 -0700

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,1 @@
-if Rails.env.production?
-  puts "production: #{ENV['REDIS_URL']}"
-  uri = URI.parse(ENV["REDIS_URL"])
-else
-  puts "not production"
-  uri = URI.parse("redis://localhost:6379")
-end
+redis = Redis.new(url: ENV["REDIS_URL"])
