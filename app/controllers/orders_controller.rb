c9648b87c31b500426c84692a75655494ca5f574
Tim <timothystahl@gmail.com>
2018-02-03 13:44:49 -0700

    modified orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,67 @@
 class OrdersController < ApplicationController
-before_action :authenticate_user!
 
-	def index
-	end
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
-	def show
-	end
+  # GET /orders
+  # GET /orders.json
+  def index
+    @orders = Order.where('user_id = ?', current_user.id)
+  end
 
-	def new
-	end
+  # GET /orders/1
+  # GET /orders/1.json
+  def show
+  end
 
-	def create
-	end
+  # GET /orders/new
+  def new
+  end
 
-	def destroy
-	end
+  # GET /orders/1/edit
+  def edit
+  end
 
-end 
+  # POST /orders
+  # POST /orders.json
+  def create
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id 
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to orders_url, notice: 'Order was successfully created.' }
+        format.json { render :index, status: :created, location: @order }
+      else
+        format.html { redirect_to orders_url, notice: 'Order was not successfully created.' }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+
+  def update
+
+  end
+
+  # DELETE /orders/1
+  # DELETE /orders/1.json
+  def destroy
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    def order_params
+      params.require(:order).permit(:product_id, :total)
+    end
+  end
